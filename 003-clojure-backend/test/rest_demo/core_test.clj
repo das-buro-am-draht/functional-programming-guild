dded8c179a9bb0a2697304a3db6685167fc00ba8
Steffen Lehn <stf@dasburo.com>
2022-07-11 12:49:20 +0200

    test: add 4 tests for exclude own channel func

--- 003-clojure-backend/test/rest_demo/core_test.clj
+++ 003-clojure-backend/test/rest_demo/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [rest-demo.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest exclude-own-channel-empty-test
+  (testing "exclude-own-channel with empty user-channel"
+    (is (= (count (rest-demo.core/excludeOwnChannel {} "user-channel")) 0))))
+
+(deftest exclude-own-channel-filled-test
+  (testing "exclude-own-channel with a filled user-channel"
+    (is (= (count (rest-demo.core/excludeOwnChannel {'foo 42} "user-channel")) 1))))
+
+(deftest exclude-own-channel-user-channel-test
+  (testing "exclude-own-channel with the user-channel"
+    (is (= (count (rest-demo.core/excludeOwnChannel {"user-channel" 42} "user-channel")) 0))))
+
+(deftest exclude-own-channel-user-and-friend-channel-test
+  (testing "exclude-own-channel with the user-channel and a friend channel"
+    (is (= (rest-demo.core/excludeOwnChannel {"user-channel" 42 "friend-channel" 77} "user-channel") (list "friend-channel")))))
